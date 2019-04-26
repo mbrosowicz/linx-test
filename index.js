5ec51b4e815c3e889c1f748cec8de38565532251
Marcelo Brosowicz de Paulo <marcelo@shiftcode.com.br>
2019-04-26 14:08:12 -0300

    Fixing css problems

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 
-app.use("/styles", express.static(__dirname + '/webapp/src/styles'));
-app.use("/js", express.static(__dirname + '/webapp/src/js'));
+app.use("/styles", express.static(__dirname + "/webapp/src/styles"));
+app.use("/js", express.static(__dirname + "/webapp/src/js"));
 
 // viewed at based directory http://localhost:8080/
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/webapp/src/index.html'));
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname + "/webapp/src/index.html"));
 });
 
 app.listen(process.env.PORT || 8080, () => {
